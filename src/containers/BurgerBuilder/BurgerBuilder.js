7e959f5565318708bd5828ee9342bd259a102c42
David Lawrie <dlawrie@att.net>
2019-08-14 19:57:00 -0500

    Layout component

---
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import Auxiliary from '../../hoc/Auxiliary';
+
+class BurgerBuilder extends Component {
+    render () {
+        return (
+            <Auxiliary>
+                <div>Burger</div>
+                <div>BuildControls</div>
+            </Auxiliary>
+        );
+    }
+}
+
+export default BurgerBuilder;
